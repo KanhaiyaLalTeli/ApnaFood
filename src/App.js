11a3fc878f767cc7e455c0dc89e9b0a8f18143b4
Kanhaiya Teli <kl9672623703@gmail.com>
2025-05-20 21:34:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+// import Header from "./components/Header";
+// import Body from "./components/Body";
+// import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+// import RestaurantMenu from "./components/RestaurantMenu";
+// import { Provider } from "react-redux";
+// import appStore from "./utils/appStore";
+// import Cart from "./components/Cart";
+//import Grocery from "./components/Grocery";
+
+
+const AppLayout = () => {
+
+
+  return (
+    <div>
+      <h1>This is react application</h1>
+    </div>
+  );
+};
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
